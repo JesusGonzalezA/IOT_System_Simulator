b18c2aa5b12577771082df74281ff9e59f63251a
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-05-18 18:14:05 +0200

    Update: delete unused variables

--- src/scripts/resumen.js
+++ src/scripts/resumen.js
@@ -1,14 +1,12 @@
-const DBServer = require("../servers/dbServer")
 const { getAC } = require("./actuadores/ac")
 const { getPersiana } = require("./actuadores/persiana")
 const { getSensores } = require("./sensores/sensores")
 
 const getResumen =  async () => {
-    
-    const db = DBServer.getInstance()
     const sensores = await getSensores()
     const ac       = await getAC()
     const persiana = await getPersiana()
+
     return {
         sensores,
         ac,

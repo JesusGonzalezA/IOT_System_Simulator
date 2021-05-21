6ff2d9f6df5d53a4b6d09729f4dc4f8c0744f6fa
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-05-21 13:44:21 +0200

    Update routes

--- src/data/actions.js
+++ src/data/actions.js
@@ -6,7 +6,8 @@ const actions = {
     TOGGLE_AC      : `${ base_uri }/ac`,
     SUBMIT_MEDIDAS : `${ base_uri }/submit-medidas`,
     GET_HISTORICO  : `${ base_uri }/get-historico`,
-    GET_RESUMEN    : `${ base_uri }/get-resumen`
+    GET_RESUMEN    : `${ base_uri }/get-resumen`,
+    LOGIN          : `${ base_uri }/login`
 }
 
 //**************************************************************************

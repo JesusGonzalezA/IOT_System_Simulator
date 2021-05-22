36ad6e1ff7fcb28f0d3918d8e11fb656b3bf6116
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-05-22 17:50:48 +0200

    ui sensors via socket in actuadores page

--- public/js/actuadores/defaultValues.js
+++ public/js/actuadores/defaultValues.js
@@ -1,8 +1,6 @@
-import { sendPost } from '../helpers/sendPost.js'
 import { sendGet } from '../helpers/sendGet.js'
 import { getName } from '../helpers/getName.js'
 
-const form         = document.getElementById('form')
 const pLuminosity  = document.getElementById('luminosidad')
 const pTemperature = document.getElementById('temperatura')
 const formPersiana = document.getElementById('form-persiana')
@@ -14,7 +12,6 @@ name.innerText = 'Casa de ' + getName()
 
 //**************************************************************************
 // Update values
-
 
 const response = await sendGet( 'action/get-resumen' )
 if ( response.status === 200) {
@@ -31,7 +28,7 @@ if ( response.status === 200) {
         formPersiana.checked = data.persiana.state
     
     if ( data.ac !== undefined )
-        formAC.checked = data.persiana.state
+        formAC.checked = data.ac.state
 } else {
     Swal.fire({
         title: 'Error',
@@ -39,3 +36,28 @@ if ( response.status === 200) {
         icon: 'error'
     })
 }
+
+//**************************************************************************
+// Update via socket
+
+import { getSession } from '../helpers/getSession.js'
+import { baseURL } from '../helpers/baseUrl.js'
+
+const socket = io.connect( baseURL )
+socket.on('connect', () => {
+
+    socket.emit('start-session', { sessionId: getSession(), name: getName() })
+
+    socket.on('set-session-acknowledgment', () => {
+
+        socket.on('avalaible-update-ac', ( value ) => {
+            console.log("AC", value)
+            formAC.checked = value
+        })
+
+        socket.on('avalaible-update-persiana', ( value ) => {
+            formPersiana.checked = value
+        })
+
+    })
+});
